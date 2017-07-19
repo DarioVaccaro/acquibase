0326da1223d6030e7e150b9aca6af821294adbcb
Dario Vaccaro <dvaccaro@hartenergy.com>
2017-07-19 16:35:41 -0500

    Created controller for accessing url path

--- public/js/controllers/acquibasecontroller.js
+++ public/js/controllers/acquibasecontroller.js
@@ -1,4 +1,4 @@
-acquibaseApp.controller('acquibaseController' , ['$scope' , '$http' , 'acquibaseFactory', function($scope , $http , acquibaseFactory) {
+acquibaseApp.controller('dataAccessController' , ['$scope' , '$http' , 'acquibaseFactory', function($scope , $http , acquibaseFactory) {
 	acquibaseFactory.get()
 		.success(function(data) {
             angular.forEach(data, function(value, key) {
@@ -6,12 +6,14 @@ acquibaseApp.controller('acquibaseController' , ['$scope' , '$http' , 'acquibase
             })
             $scope.companys = data;
         });
-        //If URL after / is the same as a company name
-        console.log(company.company.name);
-        if($location.path() === company.company.name) {
-        	//set scope to only access the doucment with the
-        	//url path as it's name WRONG CODE
-        	$scope.thisCompany = this._id;
-        	console.log(this);
-        }
+}]);
+acquibaseApp.controller('dataRestrictController' , ['$scope' , '$http' , '$location' , function($scopr, $http, $location) {
+    //If URL after / is the same as a company name
+    console.log(company.company.name);
+    if($location.path() === company.company.name) {
+        //set scope to only access the doucment with the
+        //url path as it's name WRONG CODE
+        $scope.thisCompany = this._id;
+        console.log(this);
+    }
 }]);
