7ad30bc401a4ca8c032f15e4d9a6e2e5bf60f890
Dario Vaccaro <dvaccaro@hartenergy.com>
2017-07-22 18:35:46 -0500

    Added controller that sets scope to acquisition object and adds the parent company as an additional child element

--- public/js/controllers/acquibasecontroller.js
+++ public/js/controllers/acquibasecontroller.js
@@ -1,11 +1,17 @@
 acquibaseApp.controller('dataAccessController' , ['$scope' , '$http' , 'acquibaseFactory', function($scope , $http , acquibaseFactory) {
     acquibaseFactory.get()
         .success(function(data) {
-            angular.forEach(data, function(value, key) {
-            	$scope.acquisition = data[key].company.acquisition;
-            })
             $scope.companys = data;
-            console.log($scope.acquisition);
+            $scope.acquisitionData = [];
+            angular.forEach(data, function(value, key) {
+                $scope.acquisition = $scope.companys[key].company.acquisition;
+                $scope.acquisition.parentCompany = $scope.companys[key].company.name;
+                angular.forEach($scope.acquisition, function(value, key) {
+                    value.parentCompany = $scope.acquisition.parentCompany;
+                    $scope.acquisitionData[$scope.acquisitionData.length] = value;
+                });
+            })
+            //orderBy $scope.acquisitionData
         });
 }]);
 acquibaseApp.controller('dataRestrictController' , ['$scope' , '$http' , '$location' , 'acquibaseFactory', function($scope , $http, $location , acquibaseFactory) {
--- public/js/directives/homeupdaterow.js
+++ public/js/directives/homeupdaterow.js
@@ -1,7 +1,9 @@
 acquibaseApp.directive('updateCard' , function() {
 	return {
-		restrict: 'E',
 		scope: true,
 		templateUrl: '/js/directives/homeupdaterow.html'
+		// link: function(scope , element , attrs) {
+		// 	scope.companys;
+		// }
 	}
 });
